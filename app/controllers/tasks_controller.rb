5767627f3c5fcabe3c299a99646722d2ef12db6a
Min Xuan Ng <hi@ngminxuan.com>
2021-09-08 12:48:30 +0800

    able to view each task with checked status correct

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,9 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    @task = Task.find(params[:id])
+    # if task is completed, return true (to mark the checkbox)
+  end
 end
